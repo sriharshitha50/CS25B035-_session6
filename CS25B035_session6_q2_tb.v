f2ef1551060ed5b3e9732feda4ccff5213644b6c
sriharshitha50 <pharshi2911@gmail.com>
2026-09-15 17:40:58 +0530

    Create CS25B035_session6_q2_tb.v

---
+++ CS25B035_session6_q2_tb.v
@@ -0,0 +1,12 @@
+module CS25B035_session6_tb();
+reg A,B,C,D;
+wire Z;
+cs25b029_session6 uut(A,B,C,D,Z);
+initial begin
+A=1; B=0; C=0; D=0;
+#10;
+A=0; B=1; C=0; D=0;
+#10;
+$finish;
+end
+endmodule
