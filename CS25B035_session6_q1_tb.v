6658f9a1478467ebda29b0231d1187f00687b1e5
sriharshitha50 <pharshi2911@gmail.com>
2026-09-15 17:39:02 +0530

    Add testbench for cs25b029_session6 module

---
+++ CS25B035_session6_q1_tb.v
@@ -0,0 +1,16 @@
+module cs25b029_session6_tb();
+reg A,B,C,D,E;
+wire F;
+cs25b029_session6 uut(A,B,C,D,E,F);
+initial begin
+A=1; B=0; C=0; D=0; E=0;
+#10;
+A=0; B=1; C=1; D=1; E=1;
+#10;
+A=0; B=1; C=1; D=1; E=0;
+#10;
+A=1; B=0; C=1; D=1; E=1;
+#10
+$finish;
+end
+endmodule
